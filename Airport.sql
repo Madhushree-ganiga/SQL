b7fcaf8a4a36b74bf82e81a9aafa993806d560e5
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-19 08:41:57 +0530

    Add files via upload

---
+++ Airport.sql
@@ -0,0 +1,14 @@
+create schema Airport;
+
+create table airport_details(id int primary key not null,airport_name varchar(45),location varchar(45),no_of_airline int);
+select * from airport_details;
+insert into airport_details values(5,"KIA","Bnglr",10),
+                                     (6,"KIA","Bnglr",10);
+update 	airport_details set airport_name="chennai", location="chennai" where id=4;		
+select * from airport_details group by(airport_name);	
+
+create table passengers_details(p_id int primary key not null,p_name varchar(25), a_id int, foreign key(a_id) references airport_details(id));
+insert into passengers_details values(1,"Madhushree",5),
+                                     (2,"Ramya",2);
+select * from passengers_details;
+select * from passengers_details order by(p_name);
